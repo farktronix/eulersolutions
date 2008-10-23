b39fffb116551622731b248b154fe1c28e205ec0
Jacob Farkas <farkas@apple.com>
2008-10-23 15:27:46 -0700

    This problem sucks

--- 213.c
+++ 213.c
@@ -8,47 +8,24 @@
 #include <unistd.h>
 
 #define GRID_SIZE 30
+static double sProb[GRID_SIZE][GRID_SIZE];
 
-typedef enum {
-    up = 0,
-    down,
-    left,
-    right
-} direction;
-
-void printGrid (short grid[GRID_SIZE][GRID_SIZE])
+void printGrid (double grid[GRID_SIZE][GRID_SIZE])
 {
-    short empty = 0;
-    short fleas = 0;
     char ii;
     for (ii = 0; ii < GRID_SIZE; ii++) {
         char jj;
         printf("\t[ ");
         for (jj = 0; jj < GRID_SIZE; jj++) {
-            short curFleas = grid[ii][jj];
-            if (curFleas == 0) empty++;
-            fleas += curFleas;
-            printf("%03d ", curFleas);
+            double curFleas = grid[ii][jj];
+            printf("%0.4f ", curFleas);
         }
         printf("]\n");
     }
-    printf("Total fleas: %d. Empty squares: %d\n\n", fleas, empty);
+    printf("\n\n\n");
 }
 
-short numEmpty (short grid[GRID_SIZE][GRID_SIZE])
-{
-    short empty = 0;
-    char ii;
-    for (ii = 0; ii < GRID_SIZE; ii++) {
-        char jj;
-        for (jj = 0; jj < GRID_SIZE; jj++) {
-            if (grid[ii][jj] == 0) empty++;
-        }
-    }
-    return empty;
-}
-
-void initGrid (short grid[GRID_SIZE][GRID_SIZE], short num)
+void initGrid (double grid[GRID_SIZE][GRID_SIZE], double num)
 {
     char ii;
     for (ii = 0; ii < GRID_SIZE; ii++) {
@@ -59,7 +36,7 @@ void initGrid (short grid[GRID_SIZE][GRID_SIZE], short num)
     }
 }
 
-void copyGrid (short dst[GRID_SIZE][GRID_SIZE], short src[GRID_SIZE][GRID_SIZE])
+void copyGrid (double dst[GRID_SIZE][GRID_SIZE], double src[GRID_SIZE][GRID_SIZE])
 {
     char ii;
     for (ii = 0; ii < GRID_SIZE; ii++) {
@@ -70,128 +47,93 @@ void copyGrid (short dst[GRID_SIZE][GRID_SIZE], short src[GRID_SIZE][GRID_SIZE])
     }
 }
 
-static inline char isValidJump (short ii, short jj, direction dir)
+double unoccupiedSquares (double grid[GRID_SIZE][GRID_SIZE])
 {
-    switch (dir) {
-        case -1:
-            return 0;
-        case up:
-            return ii != 0;
-        case down:
-            return ii != (GRID_SIZE - 1);
-        case left:
-            return jj != 0; 
-        case right:
-            return jj != (GRID_SIZE - 1);
-    }
-    return 0;
-}
-
-void jumpFleas (short grid[GRID_SIZE][GRID_SIZE])
-{
-    short newGrid[GRID_SIZE][GRID_SIZE];
-    initGrid(newGrid, 0);
+    double total = 0.0;
     char ii;
     for (ii = 0; ii < GRID_SIZE; ii++) {
         char jj;
         for (jj = 0; jj < GRID_SIZE; jj++) {
-            short numFleas = grid[ii][jj];
-            while (numFleas--) {
-                direction dir = -1;
-                do {
-                    //dir = arc4random() % 5;
-                    dir = rand() % 5;
-                } while (!isValidJump(ii, jj, dir));
-                switch (dir) {
-                    case up:
-                        newGrid[ii-1][jj]++;
-                        break;
-                    case down:
-                        newGrid[ii+1][jj]++;
-                        break;
-                    case left:
-                        newGrid[ii][jj-1]++;
-                        break;
-                    case right:
-                        newGrid[ii][jj+1]++;
-                        break;
-                }
-            }
+            total += grid[ii][jj];
         }
     }
-    copyGrid(grid, newGrid);
+
+    return total;
 }
 
-static uint64_t avgEmpty = 0;
-static uint64_t numRuns = 0;
-static pthread_mutex_t avgLock;
-
-// returns number of empty squares
-short doOneRun (void)
+void initSquareProb (double prob[GRID_SIZE][GRID_SIZE])
 {
-    short grid[GRID_SIZE][GRID_SIZE];
-    initGrid(grid, 1);
-    char ii;
-    for (ii = 0; ii < 50; ii++) {
-        jumpFleas(grid);
+    int jj;
+    int ii;
+
+    // corners
+    prob[0][0] = 1.0/2.0;
+    prob[GRID_SIZE - 1][0] = 1.0/2.0;
+    prob[GRID_SIZE - 1][GRID_SIZE - 1] = 1.0/2.0;
+    prob[0][GRID_SIZE - 1] = 1.0/2.0;
+
+    // sides
+    for (jj = 1; jj < GRID_SIZE - 1; jj++) { // top side
+        prob[0][jj] = 1.0/3.0;
     }
-    return numEmpty(grid);
-}
+    for (jj = 1; jj < GRID_SIZE - 1; jj++) { // bottom side
+        prob[GRID_SIZE - 1][jj] = 1.0/3.0;
+    }
+    for (ii = 1; ii < GRID_SIZE - 1; ii++) { // left side
+        prob[ii][0] = 1.0/3.0;
+    }
+    for (ii = 1; ii < GRID_SIZE - 1; ii++) { // right side
+        prob[ii][GRID_SIZE - 1] = 1.0/3.0;
+    }
 
-void *threadRunner (void *arg)
-{
-    printf("Thread %p starting up...\n", pthread_self());
-    int reportInterval = 2000 + (rand() % 3000);
-    while (1) {
-        int curNumRuns = 0;
-        int curAvgEmpty = 0; 
-        while (curNumRuns < reportInterval) {
-            curAvgEmpty += doOneRun(); 
-            curNumRuns++;
+    // inside
+    for (ii = 1; ii < GRID_SIZE - 1; ii++) {
+        for (jj = 1; jj < GRID_SIZE - 1; jj++) {
+            prob[ii][jj] = 1.0/4.0;
         }
-        pthread_mutex_lock(&avgLock);
-        avgEmpty += curAvgEmpty;
-        numRuns += curNumRuns;
-        pthread_mutex_unlock(&avgLock);
+    }   
+}
+
+double expectedFleas (double grid[GRID_SIZE][GRID_SIZE], int ii, int jj) {
+    if (ii < 0 || jj < 0 || ii >= GRID_SIZE || jj >= GRID_SIZE) return 0;
+    return grid[ii][jj] * sProb[ii][jj];
+}
+
+double emptyProb (double grid[GRID_SIZE][GRID_SIZE], int ii, int jj) {
+    if (ii < 0 || jj < 0 || ii >= GRID_SIZE || jj >= GRID_SIZE) return 0;
+    return grid[ii][jj] * (1.0 / sProb[ii][jj]);
+}
+
+void ringBell (double grid[GRID_SIZE][GRID_SIZE])
+{
+    int jj;
+    int ii;
+    double nextGrid[GRID_SIZE][GRID_SIZE];
+    initGrid(nextGrid, 0.0);
+
+    for (ii = 0; ii < GRID_SIZE; ii++) {
+        for (jj = 0; jj < GRID_SIZE; jj++) {
+            nextGrid[ii][jj] = gridProb(grid, ii - 1, jj) * gridProb(grid, ii, jj - 1) * gridProb(grid, ii + 1, jj) * gridProb(grid, ii, jj + 1);
+        }
     }
-    return NULL;
+
+    copyGrid(grid, nextGrid);
 }
 
 int main (int argc, char *argv[]) 
 {
-    sranddev();
-    pthread_mutex_init(&avgLock, NULL);
+    double grid[GRID_SIZE][GRID_SIZE];
+    initGrid(grid, 1.0);
+    initSquareProb(sProb);
 
-    int fd = open("/tmp/runstats", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    uint64_t buf = 0;
-    if (read(fd, &buf, sizeof(uint64_t))) {
-        avgEmpty = buf;
-    }
-    if (read(fd, &buf, sizeof(uint64_t))) {
-        numRuns = buf;
-    }
-    printf("Starting with %llu total empties and %llu runs\n", avgEmpty, numRuns);
-
-    pthread_t thread = NULL;
-    pthread_create(&thread, NULL, threadRunner, NULL);
-    pthread_create(&thread, NULL, threadRunner, NULL);
-    pthread_create(&thread, NULL, threadRunner, NULL);
-    pthread_create(&thread, NULL, threadRunner, NULL);
-
-
-    uint64_t lastRuns = 0;
-    while (1) {
-        sleep(5);
-        pthread_mutex_lock(&avgLock);
-        printf("Average empties: %0.07f (%llu/%llu). %0.1f runs per second\n", (double)avgEmpty/(double)numRuns, avgEmpty, numRuns, (numRuns - lastRuns)/3.0);
-        lastRuns = numRuns;
-        lseek(fd, 0, SEEK_SET);
-        write(fd, &avgEmpty, sizeof(uint64_t));
-        write(fd, &numRuns, sizeof(uint64_t));
-        pthread_mutex_unlock(&avgLock);
+    int iteration = 0;
+    while (iteration < 50) {
+        printGrid(grid);
+        ringBell(grid);         
+        iteration++;
     }
 
-    close(fd);
+    printf("Number of unoccupied squares: %0.07f \n", unoccupiedSquares(grid));
 
     return 0;
 }
