7bd919cdc3f6c9120bfda83c7eb53cd5cc43ced5
Jacob Farkas <farkas@apple.com>
2008-10-31 01:16:03 -0700

    Adding Problem 32 (not done)

---
+++ Euler/Problem32.m
@@ -0,0 +1,23 @@
+//
+//  Problem32.m
+//  Euler
+//
+//  Created by Jacob Farkas on 10/29/08.
+//  Copyright 2008 Apple Computer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PEProblem.h"
+
+@interface Problem32 : PEProblem {
+}
+@end
+
+@implementation Problem32
+- (NSString *) runSolution
+{
+
+}
+
+- (NSString *) realAnswer { return nil; }
+@end
